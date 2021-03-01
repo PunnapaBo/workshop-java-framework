ebd21d1a054e2b5e38571001fa1deade9ef1bdb2
Punnapa Boo <punnapa.bo@kbtg.tech>
2021-03-01 14:08:26 +0700

    loose coupling

--- kbtg/src/main/java/com/example/kbtg/DemoService.java
+++ kbtg/src/main/java/com/example/kbtg/DemoService.java
@@ -1,8 +1,13 @@
 package com.example.kbtg;
 
+import lombok.Data;
+
 import java.util.Random;
 
+@Data
 public class DemoService {
+    private  Random random;
+
     public String generateData(String name ){
         Random random = new Random();
         int number = random.nextInt(10);
@@ -10,7 +15,9 @@ public class DemoService {
     }
 
     public static void main(String[] args) {
+        Random random = new Random();
         DemoService demoService = new DemoService();
+        demoService.setRandom(random);
         System.out.println(demoService.generateData("punnapa"));
     }
 }

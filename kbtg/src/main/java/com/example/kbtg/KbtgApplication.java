9116079612b2ed84d025f350af5bf44f6cb620cf
Punnapa Boo <punnapa.bo@kbtg.tech>
2021-03-01 11:52:07 +0700

    Show all bean from spring context

--- kbtg/src/main/java/com/example/kbtg/KbtgApplication.java
+++ kbtg/src/main/java/com/example/kbtg/KbtgApplication.java
@@ -2,12 +2,19 @@ package com.example.kbtg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class KbtgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KbtgApplication.class, args);
+		//IoC Container
+		ConfigurableApplicationContext context = SpringApplication.run(KbtgApplication.class, args);
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			System.out.println(beanDefinitionName);
+		}
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
